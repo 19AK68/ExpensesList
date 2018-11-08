2ef2a084a9b9e06a53a59686e97c012346320cb4
Andrey Kushnaryov <akushnaryov@rightandabove.com>
2018-11-08 11:57:10 +0200

    dbContract and DbHelper are ready

--- app/src/main/java/com/ak/user/expenseslist/DataBase/DbHelper.java
+++ app/src/main/java/com/ak/user/expenseslist/DataBase/DbHelper.java
@@ -3,12 +3,14 @@ package com.ak.user.expenseslist.DataBase;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.ak.user.expenseslist.DataBase.Model.SPROperationType;
 
 public class DbHelper extends SQLiteOpenHelper
 {
 
+    private static final String TAG = "LOG";
     public static final String DB_NAME = "money.db";
     public static final int DB_VERSION = 1;
 
@@ -32,12 +34,24 @@ public class DbHelper extends SQLiteOpenHelper
         db.execSQL(DbContract.SPROperationType.CREATE_spr_operationType);
         db.execSQL(DbContract.SPROperationSource.CREATE_spr_operationSource);
         db.execSQL(DbContract.SPRCurrency.CREATE_spr_Currency);
+        db.execSQL(DbContract.Operation.DB_CREATE);
 
+        Log.d("LOG","------ OnCreate DbHelper is called db v." + db.getVersion()+"----------");
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        Log.d("LOG","------ onUpgrade DbHelper is called and upgrade  from db v." + oldVersion + "to db v." + newVersion+ "----------");
+
+        // Сделать проверку на версию и миграцию по данным через цикл курсок чтенрие в цикл и заполнение
+
+        db.execSQL("DROP TABLE IF EXISTS " + DbContract.SPROperationType.TABLE_NAME_OperationType);
+        db.execSQL("DROP TABLE IF EXISTS " + DbContract.SPROperationSource.TABLE_NAME_OperationSource);
+        db.execSQL("DROP TABLE IF EXISTS " + DbContract.SPRCurrency.TABLE_NAME_Currency);
+        db.execSQL("DROP TABLE IF EXISTS " + DbContract.Operation.TABLE_NAME_OPERATION);
+
 
     }
 }
--- app/src/main/java/com/ak/user/expenseslist/DataBase/Model/Operation.java
+++ app/src/main/java/com/ak/user/expenseslist/DataBase/Model/Operation.java
@@ -15,8 +15,59 @@ public class Operation
     public Operation(int id,String date_operation,int amount, int type_id,int currency_id,int source_id)
 
     {
-
+        this.id=id;
+        this.date_operation=date_operation;
+        this.amount=amount;
+        this.type_id=type_id;
+        this.currency_id=currency_id;
+        this.source_id=source_id;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate_operation() {
+        return date_operation;
+    }
+
+    public void setDate_operation(String date_operation) {
+        this.date_operation = date_operation;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
+
+    public int getCurrency_id() {
+        return currency_id;
+    }
+
+    public void setCurrency_id(int currency_id) {
+        this.currency_id = currency_id;
+    }
+
+    public int getSource_id() {
+        return source_id;
+    }
+
+    public void setSource_id(int source_id) {
+        this.source_id = source_id;
+    }
 }
